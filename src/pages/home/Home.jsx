68d271519b206827e8b8d0e3aad88ff331814afa
blairesc <blaireswabyclacken@yahoo.com>
2023-01-13 17:00:09 -0500

    create top navigation bar menu

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,17 @@
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
+import NavBar from "../../components/navbar/NavBar";
 
 const Home = () => {
   return (
     <div>
-        <div className="home">
-            <Sidebar />
-            <div className="homeContainer"></div>
+      <div className="home">
+        <Sidebar />
+        <div className="homeContainer">
+          <NavBar />
+          <div className="homeContainer"></div>
         </div>
+      </div>
     </div>
   )
 }
